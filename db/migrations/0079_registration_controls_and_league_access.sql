2e327a8c3be0acd1e61deaaaae70eab9884586a8
Farid <faridhosseinpour23@gmail.com>
2026-09-06 23:59:54 +0330

    fix deploy bug

--- db/migrations/0079_registration_controls_and_league_access.sql
+++ db/migrations/0079_registration_controls_and_league_access.sql
@@ -5,9 +5,9 @@ alter table public.auth_settings
 create or replace view public.public_auth_options with (security_invoker = false) as
 select otp_login_enabled,password_login_enabled,email_magic_login_enabled,
   email_signup_enabled,phone_signup_enabled,show_registration_link,
-  registration_documents_enabled,manual_account_approval_enabled,
   online_payment_enabled,card_to_card_enabled,bank_card_number,bank_iban,
-  bank_account_owner,payment_provider
+  bank_account_owner,
+  registration_documents_enabled,manual_account_approval_enabled,payment_provider
 from public.auth_settings where id=1;
 grant select on public.public_auth_options to anon,authenticated;
 
